024e3a654cadf60709b39534159c82cb25fcdaad
Pirson Bethancourt <pirsonbethancourt039@gmail.com>
2024-09-22 21:55:25 -0400

    add ReLu, SoftMax and perform forward pass through the network

--- main.py
+++ main.py
@@ -3,14 +3,24 @@
 import nnfs
 import matplotlib.pyplot as plt
 from nnfs.datasets import spiral_data
-from nn import Layer_Dense
+from nn import Layer_Dense, Activation_ReLu, Activation_Softmax
 
 nnfs.init()
 
 x, y = spiral_data(samples=100, classes=3)
 dense1 = Layer_Dense(2, 3)
+activation_1 = Activation_ReLu()
+
+dense2 = Layer_Dense(3, 3)
+
+activation_2 = Activation_Softmax()
 dense1.forward(x)
-print(dense1.output[:5])
+activation_1.forward(dense1.output)
+dense2.forward(activation_1.output)
+activation_2.forward(dense2.output)
+print(activation_2.output[:5])
+
+
 '''
 X, y = spiral_data(samples=100, classes=3)
 plt.scatter(X[:, 0], X[:, 1], c=y, cmap='brg')
--- nn.py
+++ nn.py
@@ -1,4 +1,26 @@
 import numpy as np
+import math
+
+class Activation_Softmax:
+    def forward(self, inputs):
+        '''
+        exp_values = []
+        for input in inputs:
+            exp_values.append(math.e ** input)
+        # The probabilites 
+        confidence_values = []
+        sum_of_exp = sum(exp_values)
+        for exp_value in exp_values:
+            confidence_values.append(exp_value / sum_of_exp)
+        '''
+        exp_values = np.exp(inputs - np.max(inputs, axis=1, keepdims=True))
+        probabilites = exp_values / np.sum(exp_values, axis=1, keepdims=True)
+        self.output = probabilites
+
+class Activation_ReLu:
+    def forward(self, inputs):
+        # Iterate every element of the list and append if more than 0
+        self.output = np.maximum(0, inputs)
 class Layer_Dense:
     def __init__(self, n_inputs, n_neurons):
         self.weights = 0.01 * np.random.randn(n_inputs, n_neurons)
