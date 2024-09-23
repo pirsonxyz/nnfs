03c31df03d83d40af061e3f48a9e52eb9556fbc6
Pirson Bethancourt <pirsonbethancourt039@gmail.com>
2024-09-23 17:13:30 -0400

    remove comments

--- main.py
+++ main.py
@@ -10,14 +10,6 @@ dense1 = Layer_Dense(2, 3)
 activation_1 = Activation_ReLu()
 dense2 = Layer_Dense(3, 3)
 activation_2 = Activation_Softmax()
-'''
-dense1.forward(x)
-activation_1.forward(dense1.output)
-dense2.forward(activation_1.output)
-activation_2.forward(dense2.output)
-print(activation_2.output[:5])
-'''
-
 lowest_loss = 9999999
 loss_fn = Loss_CategoricalCrossentropy()
 
