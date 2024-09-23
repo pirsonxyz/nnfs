cd05ab6b7f6ed845265688194dd91bf46e81fb45
Pirson Bethancourt <pirsonbethancourt039@gmail.com>
2024-09-23 11:34:02 -0400

    accuracy, loss classes and typing

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python
-
+import numpy as np
+import math
 import nnfs
 import matplotlib.pyplot as plt
 from nnfs.datasets import spiral_data
-from nn import Layer_Dense, Activation_ReLu, Activation_Softmax
+from nn import Layer_Dense, Activation_ReLu, Activation_Softmax, Loss_CategoricalCrossentropy, acc
 
 nnfs.init()
 
@@ -19,24 +20,10 @@ activation_1.forward(dense1.output)
 dense2.forward(activation_1.output)
 activation_2.forward(dense2.output)
 print(activation_2.output[:5])
+loss_fn = Loss_CategoricalCrossentropy()
+loss = loss_fn.calculate(activation_2.output, y)
+print('loss:', loss)
+accuracy = acc(activation_2, y)
+print('acc: ', accuracy)
 
 
-'''
-X, y = spiral_data(samples=100, classes=3)
-plt.scatter(X[:, 0], X[:, 1], c=y, cmap='brg')
-plt.show()
-inputs = [[1.0, 2.0, 3.0, 2.5], 
-          [2.0, 5.0, -1.0, 2.0], 
-          [-1.5, 2.7, 3.3, -0.8]]
-weights = [[0.2, 0.8, -0.5, 1.0], 
-           [0.5, -0.91, 0.26, -0.5], 
-           [-0.26, -0.27, 0.17, 0.87]]
-biases = [2.0, 3.0, 0.5]
-layer1_outputs = np.dot(inputs, np.array(weights).T) + biases
-weights2 = [[0.1, -0.14, 0.5], 
-           [-0.5, 0.12, -0.33], 
-           [-0.44, 0.73, -0.13]]
-biases2 = [-1, 2 , -0.5]
-layer2_outputs = np.dot(layer1_outputs, np.array(weights2).T) + biases2
-print(layer2_outputs)
-'''
--- nn.py
+++ nn.py
@@ -1,8 +1,32 @@
+from typing import Any
 import numpy as np
 import math
 
+from numpy._typing import NDArray
+
+class Loss:
+    def forward(self, y_pred, y_true) -> NDArray[Any]:
+        pass
+    def calculate(self, output: np.ndarray, y: np.ndarray) -> np.floating[Any]:
+        sample_losses = self.forward(output, y)
+        data_loss = np.mean(sample_losses)
+        return data_loss
+class Loss_CategoricalCrossentropy(Loss):
+    def forward(self, y_pred: np.ndarray, y_true: np.ndarray) -> NDArray[Any]:
+        samples = len(y_pred)
+        y_pred_clipped = np.clip(y_pred, 1e-7, 1 - 1e-7)
+        if len(y_true.shape) == 1:
+            correct_confidences = y_pred_clipped[range(samples), y_true
+        ]
+        elif len(y_true.shape) == 2:
+            correct_confidences = np.sum(y_pred_clipped * y_true,
+                                         axis=1
+            )
+        negative_loss_likelihoods = -np.log(correct_confidences)
+        return negative_loss_likelihoods
+
 class Activation_Softmax:
-    def forward(self, inputs):
+    def forward(self, inputs: np.ndarray):
         '''
         exp_values = []
         for input in inputs:
@@ -18,12 +42,17 @@ class Activation_Softmax:
         self.output = probabilites
 
 class Activation_ReLu:
-    def forward(self, inputs):
+    def forward(self, inputs: np.ndarray):
         # Iterate every element of the list and append if more than 0
         self.output = np.maximum(0, inputs)
 class Layer_Dense:
-    def __init__(self, n_inputs, n_neurons):
+    def __init__(self, n_inputs: int, n_neurons: int):
         self.weights = 0.01 * np.random.randn(n_inputs, n_neurons)
         self.biases = np.zeros((1, n_neurons))
-    def forward(self, inputs):
+    def forward(self, inputs: np.ndarray):
         self.output = np.dot(inputs, self.weights) + self.biases
+def acc(softmax_output: Activation_Softmax, y: np.ndarray) -> float:
+    preds = np.argmax(softmax_output.output, axis=1)
+    if len(y.shape) == 2:
+        y = np.argmax(y, axis=1)
+    return np.mean(preds==y)
